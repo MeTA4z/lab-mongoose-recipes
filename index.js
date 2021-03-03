3af4936aa6914efb68b5806c275b439bfeb562ad
Rafael Rodriguez <rafaelarodriguez1126@gmail.com>
2021-03-03 11:07:13 -0500

    indexed

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+
+
 const mongoose = require('mongoose');
 
-// Import of the model Recipe from './models/Recipe.model.js'
+
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
+
 const data = require('./data');
+const MONGODB_URI = "mongodb+srv://victor2:victorvictor@cluster0.hzjcy.mongodb.net/myRecipe?retryWrites=true&w=majority"
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
-// Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
@@ -16,31 +17,34 @@ mongoose
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
+   
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-
-Recipe.create({
-  title: "delicacy",
-  level: "masterchef",
-  ingredients: ["frog legs", "eggs"],
-  cuisine: "Florida",
-  dishType: "appetizer"
-  duration:: 17,
-  creator: "Rafa" 
-}).then(console.log)
+  .then(async() => {
+  
 
 
+    await Recipe.create({
+      title: "delicacy",
+      level: "masterchef",
+      ingredients: ["froglegs", "eggs"],
+      cuisine: "Florida",
+      dishType: "Snack on the steak",
+      duration: 17,
+      creator: "almighty Victor"
+    }).then(console.log)
+
+    await Recipe.insertMany(data).then(console.log)
+
+    await Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100}).then(console.log)
+
+
+    await Recipe.deleteOne({title:'Carrot Cake'}).then((res) => console.log('Carrot Cake Deleted'))
+    
+    mongoose.connection.close()
 
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
+    
   });
-
-// Iteration 2
-
-Model.create({ name: 'NewFood', duration: 32 })
-.then(res => console.log(res))
-.catch(err => console.error(err))
